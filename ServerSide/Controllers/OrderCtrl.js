7d6dd2b83de6a8997256db0624bd2fa7cc2d34cf
Ashiq Bhatti <ashiqbhatti217@gmail.com>
2025-01-29 14:57:41 +0500

    Update OrderCtrl.js frontend url

--- ServerSide/Controllers/OrderCtrl.js
+++ ServerSide/Controllers/OrderCtrl.js
@@ -7,7 +7,8 @@ dotenv.config();
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 const PlaceOrder = async (req, res) => {
-  const frontEnd_url = "http://localhost:5173";
+  // const frontEnd_url = "http://localhost:5173";
+  const frontEnd_url = "https://exclusiveab.vercel.app/";
   try {
     const newOrder = new OrderModel({
       userId: req.body.userId,

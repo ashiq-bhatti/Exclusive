a82a83a01d39919e777395fca3af77e905c1e633
ashiq-bhatti <ashiqbhatti217@gmail.com>
2025-01-29 19:07:49 +0500

    a

--- ServerSide/server.js
+++ ServerSide/server.js
@@ -16,12 +16,12 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://exclusiveclientside.vercel.app"],
-    credentials: true,  
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173", "https://exclusiveclientside.vercel.app"],
+//     credentials: true,  
+//   })
+// );
 app.use(cookieParser());
 app.use(express.json());
 DbConnection();

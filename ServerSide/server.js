203ac8a107cf41ec2f222eeceb5811e6fc2bd770
ashiq-bhatti <ashiqbhatti217@gmail.com>
2025-02-01 11:27:54 +0500

    a

--- ServerSide/server.js
+++ ServerSide/server.js
@@ -11,36 +11,35 @@ const EdditProfileRoutes = require("./Routes/EditProfileRoutes.js");
 const CartRoutes = require("./Routes/CartRoutes.js");
 const OrderRoutes = require("./Routes/OrderRoutes.js");
 const wishListRoutes = require("./Routes/WishListRoutes.js");
-const  path  = require("path");
+const path = require("path");
 
 dotenv.config();
 const app = express();
-
 const PORT = process.env.PORT || 4000;
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173", "https://exclusiveclientside.vercel.app"],
-//     credentials: true,  
-//   })
-// );
-app.use(cors());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://exclusiveclientside.vercel.app"], 
+    credentials: true,  
+    methods: "GET,POST,PUT,DELETE",
+  })
+);
+
 app.use(cookieParser());
 app.use(express.json());
 DbConnection();
 
-const __dirname = path.resolve(); // Get the absolute path
-
 app.use("/public", express.static(path.join(__dirname, "public")));
-
 
 app.use("/api/auth", AuthRoutes);
 app.use("/api/admin", AdminRoute);
 app.use("/api/product", ProductRoute);
 app.use("/api/contact", ContactRoutes);
-app.use("api/profile", EdditProfileRoutes);
+app.use("/api/profile", EdditProfileRoutes); 
 app.use("/api/cart", CartRoutes);
 app.use("/api/order", OrderRoutes);
 app.use("/api/wishlist", wishListRoutes);
+
 app.get("/", (req, res) => {
   res.send("Welcome to Server Home Page");
 });
